01f56e06e04a5c380becb7fa56b964872d95b104
Anthony Ameh <antonineutron@gmail.com>
2025-03-26 00:57:01 +0100

    Landing page done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <ReactQueryProvider>
           {children}
-        </ReactQueryProvider>
       </body>
     </html>
   )
